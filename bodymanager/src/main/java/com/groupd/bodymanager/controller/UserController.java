dcbfc52085690537ba7116904c01fe78a9d7da74
Newlimited <sbh2043@naver.com>
2023-05-08 21:23:28 +0900

    make a config

--- bodymanager/src/main/java/com/groupd/bodymanager/controller/UserController.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/controller/UserController.java
@@ -3,9 +3,14 @@ package com.groupd.bodymanager.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.groupd.bodymanager.service.UserService;
+
 @RestController
 @RequestMapping("/api/v1/user")
-public class UserController {
+public class UserController{
     
-    public Usercontroller(UserService userService)
-}
+    public UserController(UserService userService){
+        
+    }
+
+} 
