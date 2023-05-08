25235655317a15724d207e75cbe3bef987f72cfc
Newlimited <sbh2043@naver.com>
2023-05-08 22:21:00 +0900

    last push

--- bodymanager/src/main/java/com/groupd/bodymanager/dto/response/user/GetAuthResponseDto.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/dto/response/user/GetAuthResponseDto.java
@@ -8,7 +8,7 @@ import com.groupd.bodymanager.dto.response.ResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetAuthResponseDto extends ResponseDto {
@@ -16,6 +16,7 @@ public class GetAuthResponseDto extends ResponseDto {
     private int userCode;
     private String token;
     private int expirationDate;  
+
     public GetAuthResponseDto(int userCode){ //회원가입
         super("SU", "Sucess");
         this.userCode = userCode;
--- bodymanager/src/main/java/com/groupd/bodymanager/entity/UserEntity.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/entity/UserEntity.java
@@ -5,8 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import com.groupd.bodymanager.dto.request.user.PostUserRequestDto;
 import com.groupd.bodymanager.dto.request.user.SignUpRequestDto;
 import com.groupd.bodymanager.dto.response.user.GetAuthResponseDto;
 
