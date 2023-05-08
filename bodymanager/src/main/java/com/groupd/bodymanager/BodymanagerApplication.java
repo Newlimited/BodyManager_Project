c7a15e1ca87d92e958825526a7caaf886244053d
Newlimited <sbh2043@naver.com>
2023-05-08 19:44:25 +0900

    test commit

---
+++ bodymanager/src/main/java/com/groupd/bodymanager/BodymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.groupd.bodymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodymanagerApplication.class, args);
+	}
+
+}
---
+++ bodymanager/src/test/java/com/groupd/bodymanager/BodymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupd.bodymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
