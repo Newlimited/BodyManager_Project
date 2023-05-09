ddbd368373ce4870ffba7db8d3f7f4d07132b815
Newlimited <sbh2043@naver.com>
2023-05-09 12:52:21 +0900

    worked BoardRepository

--- bodymanager/src/main/java/com/groupd/bodymanager/entity/resultSet/BoardListResultSet.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/entity/resultSet/BoardListResultSet.java
@@ -13,6 +13,5 @@ public interface BoardListResultSet {
     public String getBoardWriterProfileImageUrl();
     public int getCommentCount();
     public int getLikeCount();
-    
-    
+        
 }
