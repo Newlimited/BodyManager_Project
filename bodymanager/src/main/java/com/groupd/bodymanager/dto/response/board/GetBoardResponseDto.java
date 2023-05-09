186617102cc67eeeb08602af0370345d0fb7f8a8
Newlimited <sbh2043@naver.com>
2023-05-09 21:21:01 +0900

    make board ResponseDto

--- bodymanager/src/main/java/com/groupd/bodymanager/dto/response/board/GetBoardResponseDto.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/dto/response/board/GetBoardResponseDto.java
@@ -1,10 +1,8 @@
 package com.groupd.bodymanager.dto.response.board;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.groupd.bodymanager.dto.response.ResponseDto;
-import com.groupd.bodymanager.entity.resultSet.BoardListResultSet;
+import com.groupd.bodymanager.entity.BoardEntity;
+import com.groupd.bodymanager.entity.UserEntity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,46 +10,28 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class GetBoardResponseDto extends ResponseDto{
-    private List<BoardSummary> boardList;
-
-    public GetBoardResponseDto(List<BoardListResultSet> resultSet){
-        super("SU","Success");
-
-        List<BoardSummary> boardList = new ArrayList<>();
-        
-        for(BoardListResultSet result : resultSet) {
-            BoardSummary boardSummary = new BoardSummary(result);
-            boardList.add(boardSummary);
-        }
-        this.boardList = boardList;
-    }
-}
-
-@Data
 @AllArgsConstructor
-@NoArgsConstructor
-class BoardSummary {
-   
-    private Integer boardNumber;
+// 특정 게시물 조회
+public class GetBoardResponseDto extends ResponseDto{
+    private int boardNumber;
     private String boardTitle;
-    private String boardContent;
     private String boardImageUrl;
     private String boardWriteDatetime;
     private int viewCount;
     private String boardWriterEmail;
     private String boardWriterNickname;
+   
 
-    public BoardSummary(BoardListResultSet resultSet){
-    this.boardNumber = resultSet.getBoardNumber();
-    this.boardTitle =resultSet.getBoardTitle();
-    this.boardContent=resultSet.getBoardContent();
-    this.boardImageUrl=resultSet.getBoardimageUrl();
-    this.boardWriteDatetime = resultSet.getBoardWrtieDatetime();
-    this.viewCount = resultSet.getViewCount();
-    this.boardWriterEmail = resultSet.getBoardWriterEmail();
-    this.boardWriterNickname = resultSet.getBoardWriterNickname();
-
+    public GetBoardResponseDto(BoardEntity boardEntity, UserEntity userEntity
+    ){
+        super("SU","Success");
+        this.boardNumber = boardEntity.getBoradNumber();
+        this.boardTitle = boardEntity.getBoardTitle();
+        this.boardImageUrl = boardEntity.getBoardImageUrl();
+        this.boardWriteDatetime = boardEntity.getBoradWriteDatetime();
+        this.viewCount = boardEntity.getViewCount();
+        this.boardWriterEmail = userEntity.getUserEmail();
+        this.boardWriterNickname = userEntity.getUserNickname();
+        
     }
-
 }
--- bodymanager/src/main/java/com/groupd/bodymanager/service/implement/BoardServiceImplement.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/service/implement/BoardServiceImplement.java
@@ -1,5 +1,76 @@
 package com.groupd.bodymanager.service.implement;
 
-public class BoardServiceImplement {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.groupd.bodymanager.dto.request.board.PatchBoardRequestDto;
+import com.groupd.bodymanager.dto.request.board.PostBoardRequestDto;
+import com.groupd.bodymanager.dto.response.ResponseDto;
+import com.groupd.bodymanager.dto.response.board.GetBoardListResponseDto;
+import com.groupd.bodymanager.dto.response.board.GetBoardResponseDto;
+import com.groupd.bodymanager.entity.BoardEntity;
+import com.groupd.bodymanager.repository.BoardRepository;
+import com.groupd.bodymanager.repository.UserRepository;
+import com.groupd.bodymanager.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImplement implements BoardService{
+    
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+    
+    public ResponseEntity<ResponseDto> postBoard(PostBoardRequestDto dto) {
+        ResponseDto body = null;
+
+        String boardWriterEmail = dto.getBoardWriterEmail();
+// 의논 해야 할 것
+        try {
+            // TODO 존재하지 않는 유저 오류 반환
+            boolean existedUserEmail = userRepository.existsByEmail(boardWriterEmail);
+            if (!existedUserEmail) {
+                ResponseDto errorbody = new ResponseDto("NU", "Non-Existent User Email");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorbody);
+            }
+            BoardEntity boardEntity = new BoardEntity(dto);
+            boardRepository.save(boardEntity);
+
+            body = new ResponseDto("Su", "Success");
+
+        } catch (Exception exception) {
+            // TODO : 데이터 베이스 오류반환
+            exception.printStackTrace();
+            ResponseDto errorbody = new ResponseDto("DE", "Database Error");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorbody);
+        }
+
+        // TODO: 성공반환
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+
+    
+    @Override
+    public ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBoard'");
+    }
+    @Override
+    public ResponseEntity<? super GetBoardListResponseDto> getBoardList() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBoardList'");
+    }
+    @Override
+    public ResponseEntity<ResponseDto> patchBoard(String userEmail, PatchBoardRequestDto dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'patchBoard'");
+    }
+    @Override
+    public ResponseEntity<ResponseDto> deleteBoard(String userEmail, Integer boardNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteBoard'");
+    }
     
 }
