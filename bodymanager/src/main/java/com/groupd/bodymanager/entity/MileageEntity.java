a8dbb4cc41e3da31757e01d3fb7652751717e174
hhosub <ghtjq1573@naver.com>
2023-05-11 04:18:41 +0900

    mileage first commit

--- bodymanager/src/main/java/com/groupd/bodymanager/entity/MileageEntity.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/entity/MileageEntity.java
@@ -1,7 +1,10 @@
 package com.groupd.bodymanager.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.groupd.bodymanager.dto.request.mileage.PostMileageRequestDto;
 
 import lombok.Data;
 
@@ -10,7 +13,10 @@ import lombok.Data;
 @Entity(name = "Mileage")
 public class MileageEntity {
 
-    private int attendanceResult;
+    public MileageEntity(PostMileageRequestDto dto) {
+    }
+    @Id
+    private boolean attendanceResult;
     private int attendanceMileage;
     private String attendanceDate;
 
