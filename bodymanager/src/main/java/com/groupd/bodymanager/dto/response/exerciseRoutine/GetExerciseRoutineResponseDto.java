28d4dad6459bf57a169d08db24c6c31e9b1bbccf
Dahye Kim <digeuthi@gmail.com>
2023-05-10 12:49:10 +0900

    exercise part

--- bodymanager/src/main/java/com/groupd/bodymanager/dto/response/exerciseRoutine/GetExerciseRoutineResponseDto.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/dto/response/exerciseRoutine/GetExerciseRoutineResponseDto.java
@@ -2,6 +2,14 @@ package com.groupd.bodymanager.dto.response.exerciseRoutine;
 
 import com.groupd.bodymanager.dto.response.ResponseDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetExerciseRoutineResponseDto extends ResponseDto{
-    
+    private String routineImageUrl;
+
 }
--- bodymanager/src/main/java/com/groupd/bodymanager/entity/ExerciseRoutineEntity.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/entity/ExerciseRoutineEntity.java
@@ -1,5 +1,22 @@
 package com.groupd.bodymanager.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Exersicse")
+@Table(name = "Exersicse")
 public class ExerciseRoutineEntity {
+    
+    @Id
+    private int routineNumber;
+    private String routineImageUrl;
     
 }
