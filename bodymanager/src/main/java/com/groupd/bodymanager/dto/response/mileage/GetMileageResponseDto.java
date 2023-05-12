3d64cfc7e71276fcff5f30752d4803e7a856195e
hhosub <ghtjq1573@naver.com>
2023-05-12 09:54:20 +0900

    2023-05-12 push

--- bodymanager/src/main/java/com/groupd/bodymanager/dto/response/mileage/GetMileageResponseDto.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/dto/response/mileage/GetMileageResponseDto.java
@@ -1,7 +1,27 @@
 package com.groupd.bodymanager.dto.response.mileage;
 
-import com.groupd.bodymanager.dto.response.ResponseDto;
+import java.util.Date;
 
+import com.groupd.bodymanager.dto.response.ResponseDto;
+import com.groupd.bodymanager.entity.MileageEntity;
+import com.groupd.bodymanager.entity.UserEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetMileageResponseDto extends ResponseDto{
-    
+    private int userCode;
+    private boolean attendanceResult;
+    private int attendanceMileage;
+    private Date attendanceDate;
+
+    public GetMileageResponseDto(MileageEntity mileageEntity){
+        super("SU", "Success");
+        this.attendanceDate = 
+    }
 }
+
