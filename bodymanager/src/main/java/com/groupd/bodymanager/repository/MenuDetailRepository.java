90c576a0069e2e7dc6b8c7ec110a537d3c99de6e
hotsolo <zxdx75@naver.com>
2023-05-16 10:48:03 +0900

    msg

--- bodymanager/src/main/java/com/groupd/bodymanager/repository/MenuDetailRepository.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/repository/MenuDetailRepository.java
@@ -2,10 +2,13 @@ package com.groupd.bodymanager.repository;
 
 import java.util.List;
 
+import org.hibernate.query.NativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.groupd.bodymanager.entity.MenuDetailEntity;
+import com.groupd.bodymanager.entity.resultSet.MenuListResultSet;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,22 +17,23 @@ public interface MenuDetailRepository extends JpaRepository<MenuDetailEntity,Int
     public MenuDetailEntity findByMenuCode(String menuCode);
 
     @Query(value = 
-    "select" 
-        MD.menu_code AS menuCode,
-        MD.time,
-        MD.monday,
-        MD.tuesday,
-        MD.wednesday,
-        MD.thursday,
-        MD.friday,
-        MD.saturday,
-        MD.sunday
-        from menu M, menu_detail MD, usermenuselect US, user U
-        where MD.menu_code = M.menu_code 
-        and M.menu_code = US.menu_code 
-        and US.user_code = U.user_code
-        order by MD.menu_index; 
-    
+        "select" 
+        +"MD.menu_code AS menuCode,"
+        +"MD.time,"
+        +"MD.monday,"
+        +"MD.tuesday,"
+        +"MD.wednesday,"
+        +"MD.thursday,"
+        +"MD.friday,"
+        +"MD.saturday,"
+        +"MD.sunday"
+        +"from menu M, menu_detail MD, usermenuselect US, user U"
+        +"where MD.menu_code = M.menu_code "
+        +"and M.menu_code = US.menu_code "
+        +"and US.user_code = U.user_code"
+        +"order by MD.menu_index;",
+        nativeQuery = true
     )
+    public List<MenuListResultSet> getMenuDetailList();
 
 }
