6b4ae24afa4df71a215e4e43b417aed8708a9f3f
hotsolo <zxdx75@naver.com>
2023-05-16 10:42:48 +0900

    msg

--- bodymanager/src/main/java/com/groupd/bodymanager/repository/MenuDetailRepository.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/repository/MenuDetailRepository.java
@@ -3,6 +3,7 @@ package com.groupd.bodymanager.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.groupd.bodymanager.entity.MenuDetailEntity;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,24 @@ import org.springframework.stereotype.Repository;
 public interface MenuDetailRepository extends JpaRepository<MenuDetailEntity,Integer>{
 
     public MenuDetailEntity findByMenuCode(String menuCode);
+
+    @Query(value = 
+    "select" 
+        MD.menu_code AS menuCode,
+        MD.time,
+        MD.monday,
+        MD.tuesday,
+        MD.wednesday,
+        MD.thursday,
+        MD.friday,
+        MD.saturday,
+        MD.sunday
+        from menu M, menu_detail MD, usermenuselect US, user U
+        where MD.menu_code = M.menu_code 
+        and M.menu_code = US.menu_code 
+        and US.user_code = U.user_code
+        order by MD.menu_index; 
     
+    )
+
 }
