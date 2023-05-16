c77eb6ee9b52508ba3dba65c43c13cae946280de
Newlimited <sbh2043@naver.com>
2023-05-17 08:41:08 +0900

    nothing

--- bodymanager/src/main/java/com/groupd/bodymanager/service/MenuService.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/service/MenuService.java
@@ -12,12 +12,7 @@ import com.groupd.bodymanager.dto.response.menu.GetMenuDetailListResponseDto;
 public interface MenuService {
   
     public ResponseEntity<ResponseDto> patchDietRoutine(PatchMenuRequestDto dto);
-<<<<<<< HEAD
-    public ResponseEntity<? super GetMenuResponseDto> getMenuDetail(PostMenuRequestDto dto);
-    public ResponseEntity<? super GetMenuDetailListResponseDto> postDietRoutine(PostMenuRequestDto dto);
-=======
     public ResponseEntity<? super GetMenuResponseDto> getDietRoutine(PostMenuRequestDto dto);
     public ResponseEntity<? super GetMenuDetailListResponseDto> getMenuDetailList();
 
->>>>>>> 0cd8678ce329d252ecbfbebf662855bbc34a9417
 }
