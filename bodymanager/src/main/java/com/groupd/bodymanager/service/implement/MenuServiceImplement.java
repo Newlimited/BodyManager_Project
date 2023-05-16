e5cf3a6f576bd1bdd7bc23d74e414a3026ce48f3
hotsolo <zxdx75@naver.com>
2023-05-16 13:04:59 +0900

    chy

--- bodymanager/src/main/java/com/groupd/bodymanager/service/implement/MenuServiceImplement.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/service/implement/MenuServiceImplement.java
@@ -64,17 +64,8 @@ public class MenuServiceImplement implements MenuService{
     }
 
     
-    @Override //메뉴코드에 맞는 식단 조회
-    public ResponseEntity<? super GetMenuDetailListResponseDto> getMenuDetailList() {
-        GetMenuDetailListResponseDto body = null;
-
-        try {
-            List<MenuListResultSet> resultSet = menuDetailRepository.getMenuDetailList();
-            body = new GetMenuDetailListResponseDto(resultSet);
-            
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return CustomResponse.databaseError();
+    
+    
         }
         return ResponseEntity.status(HttpStatus.OK).body(body);
     }
@@ -84,6 +75,7 @@ public class MenuServiceImplement implements MenuService{
         GetMenuResponseDto body = null;
         String menuCode = dto.getMenuCode();
         int userCode = dto.getUserCode();
+        
 
         try {
             MenuEntity menuEntity = menuRepository.findByUserCode(userCode);
