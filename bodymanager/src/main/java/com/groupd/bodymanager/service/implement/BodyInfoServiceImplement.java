705b9cd8a48cd04c5527891351e8bd3efe9b32bf
hhosub <ghtjq1573@naver.com>
2023-05-16 11:11:21 +0900

    testgit add .

--- bodymanager/src/main/java/com/groupd/bodymanager/service/implement/BodyInfoServiceImplement.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/service/implement/BodyInfoServiceImplement.java
@@ -22,16 +22,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class BodyInfoServiceImplement implements BodyInfoService {
-    private UserRepository userRepository;
-    private BodyInfoRepository bodyInfoRepository;
+    private final UserRepository userRepository;
+    private final BodyInfoRepository bodyInfoRepository;
 
-    @Autowired
-    public BodyInfoServiceImplement(
-        UserRepository userRepository, BodyInfoRepository bodyInfoRepository){
-            this.userRepository = userRepository;
-            this.bodyInfoRepository = bodyInfoRepository;
-    }
-    
+   
     @Override
     public ResponseEntity<ResponseDto> postBodyInfo(PostBodyInfoRequestDto dto) {
 
@@ -46,8 +40,25 @@ public class BodyInfoServiceImplement implements BodyInfoService {
                 ResponseDto errorBody = new ResponseDto("NC", "Non-Existent User Code");
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorBody);
             }
-
+            // kg/㎡. BMI가 18.5 이하면 저체중 ／ 18.5 ~ 22.9 사이면 정상 ／ 23.0 ~ 24.9 사이면 과체중 ／ 25.0 / 비만 
+            double heightForBmiIndex = dto.getHeight()/100 ;
+            double weightForBmiIndex = dto.getWeight();
+            double calculateForBmiIndex = weightForBmiIndex/(heightForBmiIndex*heightForBmiIndex);
             BodyInfoEntity bodyInfoEntity = new BodyInfoEntity(dto);
+            bodyInfoEntity.setBmiIndex(calculateForBmiIndex);
+            Double bmiReult = bodyInfoEntity.getBmiIndex();
+            if(!(bmiReult <= 18.5 )) {
+                if(!(bmiReult <= 22.9)){
+                    if(!(bmiReult <= 24.9) ){
+                        if(25.0 <= bmiReult) {
+                            bodyInfoEntity.setBmiResult("비만");
+                        }
+                        bodyInfoEntity.setBmiResult("과체중");
+                    }
+                     bodyInfoEntity.setBmiResult("정상");
+                    }
+                 bodyInfoEntity.setBmiResult("저체중");
+                }                               
             bodyInfoRepository.save(bodyInfoEntity);
 
             body = new ResponseDto("SU", "Success");
@@ -69,34 +80,31 @@ public class BodyInfoServiceImplement implements BodyInfoService {
         GetBodyInfoResponseDto body = null;
 
         try {
-            if(userCode == null) return CustomResponse.validationFaild();
+            if (userCode == null)
+                return CustomResponse.validationFaild();
             // 존재하지않는 유저코드
             BodyInfoEntity bodyInfoEntity = bodyInfoRepository.findByUserCode(userCode);
-            if(bodyInfoEntity == null) {
+            if (bodyInfoEntity == null) {
                 return CustomResponse.notExistUserCode();
             }
-            double height = bodyInfoEntity.getHeight();
-            double weight = bodyInfoEntity.getWeight();
-            double muscleMass = bodyInfoEntity.getMuscleMass();
 
-            
             UserEntity userEntity = userRepository.findByUserCode(userCode);
             body = new GetBodyInfoResponseDto(bodyInfoEntity, userEntity);
             // 미완성
-            
+
         } catch (Exception exception) {
-           // 데이터베이스 오류
+            // 데이터베이스 오류
             exception.printStackTrace();
             ResponseDto errorBody = new ResponseDto("DE", "Database Error");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorBody);
-            }
-        
+        }
+
         return ResponseEntity.status(HttpStatus.OK).body(body);
 
     }
 
-    public ResponseEntity<ResponseDto> patchBodyInfo(PatchBodyInfoRequestDto dto){
-        
+    public ResponseEntity<ResponseDto> patchBodyInfo(PatchBodyInfoRequestDto dto) {
+
         ResponseDto body = null;
         int userCode = dto.getUserCode();
         double height = dto.getHeight();
@@ -107,16 +115,15 @@ public class BodyInfoServiceImplement implements BodyInfoService {
         try {
 
             BodyInfoEntity bodyInfoEntity = bodyInfoRepository.findByUserCode(userCode);
-            if(bodyInfoEntity == null){
+            if (bodyInfoEntity == null) {
                 return CustomResponse.notExistUserCode();
             }
-            
+
             bodyInfoEntity.setHeight(height);
             bodyInfoEntity.setWeight(weight);
             bodyInfoEntity.setMuscleMass(muscleMass);
             bodyInfoEntity.setFatRate(fatRate);
             bodyInfoRepository.save(bodyInfoEntity);
-
 
         } catch (Exception exception) {
             exception.printStackTrace();
@@ -127,4 +134,3 @@ public class BodyInfoServiceImplement implements BodyInfoService {
     }
 
 }
-
