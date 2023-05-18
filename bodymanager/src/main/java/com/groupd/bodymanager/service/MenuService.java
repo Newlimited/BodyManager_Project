bab6e5cc783e993d54040ea0162f430706575aa2
hotsolo <zxdx75@naver.com>
2023-05-18 11:51:48 +0900

    chy

--- bodymanager/src/main/java/com/groupd/bodymanager/service/MenuService.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/service/MenuService.java
@@ -7,7 +7,7 @@ import com.groupd.bodymanager.dto.response.ResponseDto;
 import com.groupd.bodymanager.dto.response.menu.GetMenuDetailListResponseDto;
 
 public interface MenuService {
-  
+
     public ResponseEntity<ResponseDto> postMenuCodeAndUserCode(MenuRequestDto dto);
     public ResponseEntity<? super GetMenuDetailListResponseDto> getMenuDetailList(Integer userCode);
     public ResponseEntity<ResponseDto> patchMenuCode(MenuRequestDto dto);
