b9de21f30080ae1e4acefef5025bfb225a7adb5d
Newlimited <sbh2043@naver.com>
2023-05-18 11:13:28 +0900

    mergeing

--- SQLscript.sql
+++ SQLscript.sql
@@ -17,21 +17,6 @@ AND M.manager_email = U.user_email
 ORDER BY B.board_number DESC;
 select * from manager;
 
-SELECT
-D.user_code AS userCode,
-D.time AS Time,
-D.item AS Food,
-D.diet_number AS Day,
-D.menu_code AS counter
-FROM diet_detail D, user U, diet DI, menu ME
-WHERE D.user_code = DI.user_code
-AND DI.user_code = ME.user_code
-AND ME.user_code = U.user_code
-AND D.diet_number = DI.diet_number
-AND D.menu_code = DI.menu_code
-AND DI.menu_code = ME.menu_code
-ORDER BY D.diet_number DESC,
-D.user_code DESC;
 
 CREATE VIEW diet_menu AS (
 SELECT
