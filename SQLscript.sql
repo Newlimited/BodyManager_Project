1d185b0e11a4e0db290a0f217350d835c3fabf57
Newlimited <sbh2043@naver.com>
2023-05-22 12:19:34 +0900

    check scrpit

--- SQLscript.sql
+++ SQLscript.sql
@@ -1,18 +1,5 @@
-USE BODYMANAGER;
+use bodymanager;
 
-# USER 
-CREATE VIEW manager_info AS(
-SELECT U.user_code, U.user_email,
-U.user_nickname, U.user_age, U.user_phone_number
-FROM user U, manager M 
-WHERE U.user_email = M.manager_email
-ORDER BY user_code ASC
-);
-
-
-
-
-# BOARD
 CREATE VIEW board_view AS (
 SELECT
 B.board_number AS boardNumber,
@@ -31,10 +18,25 @@ ORDER BY B.board_number DESC
 SELECT * from board_view ;
 
 
+-- SELECT 
+-- B.board_number As 게시물번호,
+-- B.title AS boardTitle,
+-- B.content As boardContent,
+-- B.board_image_url AS boardImageUrl,
+-- B.write_datetime As boardWriteDatetime,
+-- B.view_count As viewCount,
+-- U.email As boardWriterEmail,
+-- U.nickname AS boardWriterNickname,
+-- U.profile_image_url AS boardWriterProfileImageUrl,
+-- count(DISTINCT C.comment_number) As commentCount,
+-- count(DISTINCT L.user_email) AS likeCount
 
+-- from Board B, Comment C, Liky L, User U
+-- where B.board_number = C.board_number
+-- AND B.writer_email = U.email 
+-- GROUP BY B.board_number 
+-- ORDER BY B.write_datetime DESC;
 
-
-#EXCERCISE_ROUTINE
 INSERT INTO exercise_routine VALUES (1, "routine 1", "routine 2", "routine 3");
 INSERT INTO exercise_routine VALUES (2, "routine 1", "routine 2", "routine 3");
 INSERT INTO exercise_routine VALUES (3, "routine 1", "routine 2", "routine 3");
@@ -45,21 +47,4 @@ INSERT INTO exercise_routine VALUES (3, "routine 1", "routine 2", "routine 3");
 
 
 
-# MANAGER
 
-
-
-
-# BODYINFO
-
-
-
-# MENU
-INSERT INTO menu VALUES ('A');
-INSERT INTO menu VALUES ('B');
-INSERT INTO menu VALUES ('C');
-
-
-
-
-# MILEAGE
