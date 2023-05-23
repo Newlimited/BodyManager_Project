040931fa5cdb57792f6543252d1544d0a63a7fe7
Newlimited <sbh2043@naver.com>
2023-05-23 11:50:35 +0900

    add bodyinfoList

--- bodymanager/src/main/java/com/groupd/bodymanager/dto/response/bodyInfo/GetBodyInfoResponseDto.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/dto/response/bodyInfo/GetBodyInfoResponseDto.java
@@ -1,5 +1,8 @@
 package com.groupd.bodymanager.dto.response.bodyInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.groupd.bodymanager.dto.response.ResponseDto;
 import com.groupd.bodymanager.entity.BodyInfoEntity;
 import com.groupd.bodymanager.entity.UserEntity;
@@ -8,36 +11,50 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetBodyInfoResponseDto extends ResponseDto{
+public class GetBodyInfoResponseDto extends ResponseDto {
     private Integer userCode;
-    private String userEmail;
+    private List<BodyInfoList> bodyInfoList;
+
+    public GetBodyInfoResponseDto(List<BodyInfoEntity> bodyInfoEntities, UserEntity userEntity) {
+        super("SU", "Success");
+
+        this.userCode = userEntity.getUserCode();
+        BodyInfoList bodyInfoList = new BodyInfoList();
+        this.bodyInfoList = bodyInfoList.recordList(bodyInfoEntities);
+
+    }
+}
+@Data
+@NoArgsConstructor
+class BodyInfoList {
+    private String recordDate;
     private double height;
     private double weight;
     private double muscleMass;
     private double fatRate;
-    private String recordDate;
     private double bmiIndex;
     private String bmiResult;
 
-    public GetBodyInfoResponseDto(BodyInfoEntity bodyInfoEntity,UserEntity userEntity) {
-        super("SU", "Success");
+    BodyInfoList(BodyInfoEntity bodyInfoEntity) {
 
-        this.userCode = bodyInfoEntity.getUserCode();
-        this.userEmail = userEntity.getUserEmail();
+        this.recordDate = bodyInfoEntity.getRecordDate();
         this.height = bodyInfoEntity.getHeight();
         this.weight = bodyInfoEntity.getWeight();
         this.muscleMass = bodyInfoEntity.getMuscleMass();
         this.fatRate = bodyInfoEntity.getFatRate();
-        this.recordDate = bodyInfoEntity.getRecordDate();
         this.bmiIndex = bodyInfoEntity.getBmiIndex();
         this.bmiResult = bodyInfoEntity.getBmiResult();
-
     }
+    static List<BodyInfoList> recordList(List<BodyInfoEntity> bodyInfoEntities){
+        List<BodyInfoList> bodiInfoLists = new ArrayList<>();
 
-
-    
-}  
+    for (BodyInfoEntity bodyInfoEntity : bodyInfoEntities) {
+        BodyInfoList bodyinfoList = new BodyInfoList(bodyInfoEntity);
+        bodiInfoLists.add(bodyinfoList);
+    }
+    return bodiInfoLists;
+}
+}
