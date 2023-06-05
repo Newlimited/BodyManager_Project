34ed7849b5a849235ad15c85e4887b68a8d55a69
Newlimited <sbh2043@naver.com>
2023-06-05 10:18:43 +0900

    check

--- bodymanager/src/main/java/com/groupd/bodymanager/filter/JwtAuthenticationFilter.java
+++ bodymanager/src/main/java/com/groupd/bodymanager/filter/JwtAuthenticationFilter.java
@@ -17,7 +17,6 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.groupd.bodymanager.common.CustomResponse;
 import com.groupd.bodymanager.provider.JwtProvider;
 import com.groupd.bodymanager.service.UserService;
 
